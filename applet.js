f488ff14b542d866600d519d725c27ae7e4953c1
pavanred <pavankumar.kh@gmail.com>
2012-12-14 20:34:04 -0600

    added applet to panel and main menu

--- applet.js
+++ applet.js
@@ -1,4 +1,13 @@
 const Applet = imports.ui.applet;
+const PopupMenu = imports.ui.popupMenu;
+const Gettext = imports.gettext.domain('cinnamon-extensions');
+const _ = Gettext.gettext;
+const St = imports.gi.St; // http://developer.gnome.org/st/stable/
+const AppletDir = imports.ui.appletManager.appletMeta['sports-update@pavanred'].path;
+const Mainloop = imports.mainloop;
+const Lang = imports.lang;
+
+const UUID = 'sports-update@pavanred';
 
 function MyApplet(orientation) {
     this._init(orientation);
@@ -11,27 +20,31 @@ MyApplet.prototype = {
         Applet.TextIconApplet.prototype._init.call(this, orientation);
 
         try {
-		this.set_applet_tooltip(getCurrentDescription());	
-		this.set_applet_label(getCurrentStatus());
-		this.set_applet_icon_name("force-kill");
-		//this.set_applet_icon_path(AppletDir + "/icon.png");
-        }
-        catch (e) {
+			this.set_applet_tooltip("Sports - Get live scores");	
+			this.set_applet_label("");
+			this.set_applet_icon_path(AppletDir + "/icon.png");
+		
+			this.menuManager = new PopupMenu.PopupMenuManager(this);
+		    this.menu = new Applet.AppletPopupMenu(this, orientation);
+		    this.menuManager.addMenu(this.menu); 		
+        }    
+	    catch (e) {
             global.logError(e);
         }
      },
 
     on_applet_clicked: function(event) {
-        
+        log(event);
+		this.menu.toggle();
+    },
+
+    refreshScores: function refreshScores() {	
+		log("in refreshScores()");
     }
 };
 
-function getCurrentStatus() {
-    return "Live";
-}
-
-function getCurrentDescription() {
-    return "Cricket Live: XXX 123/2";
+function log(message) {
+	global.log(UUID + "." + log.caller.name + "." + message);
 }
 
 function main(metadata, orientation) {
